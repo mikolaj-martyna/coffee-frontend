f86cda638e40ab93ff522ab3af6efb5d157f8cb5
mikolaj.martyna <mikolaj.martyna.01@gmail.com>
2024-03-09 19:41:47 +0100

    Add temp Root page

---
+++ src/root/Root.js
@@ -0,0 +1,15 @@
+import {Outlet} from "react-router-dom";
+import Button from "@mui/material/Button";
+import {Link} from 'react-router-dom';
+
+export default function Root() {
+    return (
+        <>
+            <Button component={Link} to="/login">Login</Button>
+            <Button component={Link} to="/register">Register</Button>
+            <Button component={Link} to="/checkout">Checkout</Button>
+
+            <Outlet/>
+        </>
+    );
+}
