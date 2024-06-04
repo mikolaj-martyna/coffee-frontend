ec06fe0c04bc549d1cc5cd0aa07162999a5f3607
mikolaj.martyna <mikolaj.martyna.01@gmail.com>
2024-06-04 13:16:42 +0200

    Implement add to cart functionality

--- src/product/Product.js
+++ src/product/Product.js
@@ -1,9 +1,11 @@
 // src/pages/ProductPage.jsx
-import React from 'react';
+import React, {useState} from 'react';
 import {Typography, Box, Card, CardMedia, CardContent, Button} from '@mui/material';
-import {Link} from 'react-router-dom';
 
 export default function Product(product) {
+    const [status, setStatus] = useState("");
+    const [data, setData] = useState();
+    const [isLoading, setLoading] = useState(true);
 
     return (
         <Card sx={{minWidth: 200, maxWidth: 300, marginBottom: 2, marginX: 4}}>
@@ -21,7 +23,16 @@ export default function Product(product) {
             </CardContent>
             <Box display="flex" p={2}>
                 <Typography variant="h6" pr={2}>{product.price} zł</Typography>
-                <Button variant="contained" color="primary" component={Link} to="/checkout">
+                <Button variant="contained" color="primary" onClick={async () => {
+                    await fetch("http://localhost:8080/cart/add", {
+                        method: "POST",
+                        headers: {
+                            'Authorization': 'Bearer ' + localStorage.getItem('token'),
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify([product.id])
+                    });
+                }}>
                     Add to cart
                 </Button>
             </Box>
