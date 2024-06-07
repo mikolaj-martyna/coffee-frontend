d54bec4ad4c5a4d4994f985bc0630895eaace317
mikolaj.martyna <mikolaj.martyna.01@gmail.com>
2024-06-07 12:18:39 +0200

    Implement place order functionality

--- src/checkout/Checkout.js
+++ src/checkout/Checkout.js
@@ -14,6 +14,7 @@ import Typography from '@mui/material/Typography';
 import AddressForm from './AddressForm';
 import PaymentForm from './PaymentForm';
 import Review from './Review';
+import {useState} from "react";
 
 function Copyright() {
     return (
@@ -57,6 +58,7 @@ export default function Checkout() {
         expDate: '',
         cvv: ''
     });
+    const [status, setStatus] = useState("");
 
     const handleInputChange = (name, value) => {
         setFormData({...formData, [name]: value});
@@ -74,67 +76,94 @@ export default function Checkout() {
         setActiveStep(activeStep - 1);
     };
 
-    const handleCheckout = () => {
-        // TODO: handle checkout
-    }
+    const handleCheckout = async (event) => {
+        await fetch("http://localhost:8080/user/edit", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            },
+            body: JSON.stringify({
+                name: "",
+                surname: "",
+                email: "",
+                country: formData.country,
+                city: formData.city,
+                street: formData.address1 + " " + formData.address2,
+                zipCode: formData.zipCode,
+                buildingNumber: formData.buildingNumber,
+                apartmentNumber: formData.apartmentNumber
+            })
+        });
+
+        // TODO: update card info here
+
+        await fetch("http://localhost:8080/order/create", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            }
+        });
+    };
 
     return (
         <>
-            { localStorage.getItem("token") === null ?
-            <>
-                <Typography component="h1" variant="h4" align="center">
-                    Log in to checkout
-                </Typography>
-            </>
-            :
-            <>
-                <Container component="main" maxWidth="sm" sx={{mb: 4}}>
-                    <Paper variant="outlined" sx={{my: {xs: 3, md: 6}, p: {xs: 2, md: 3}}}>
-                        <Typography component="h1" variant="h4" align="center">
-                            Checkout
-                        </Typography>
-                        <Stepper activeStep={activeStep} sx={{pt: 3, pb: 5}}>
-                            {steps.map((label) => (
-                                <Step key={label}>
-                                    <StepLabel>{label}</StepLabel>
-                                </Step>
-                            ))}
-                        </Stepper>
-                        {activeStep === steps.length ? (
-                            <React.Fragment>
-                                <Typography variant="h5" gutterBottom>
-                                    Thank you for your order.
-                                </Typography>
-                                <Typography variant="subtitle1">
-                                    Your order number is #2001539. We have emailed your order
-                                    confirmation, and will send you an update when your order has
-                                    shipped.
-                                </Typography>
-                            </React.Fragment>
-                        ) : (
-                            <React.Fragment>
-                                {getStepContent(activeStep, handleInputChange, formData)}
-                                <Box sx={{display: 'flex', justifyContent: 'flex-end'}}>
-                                    {activeStep !== 0 && (
-                                        <Button onClick={handleBack} sx={{mt: 3, ml: 1}}>
-                                            Back
-                                        </Button>
-                                    )}
+            {localStorage.getItem("token") === null ?
+                <>
+                    <Typography component="h1" variant="h4" align="center">
+                        Log in to checkout
+                    </Typography>
+                </>
+                :
+                <>
+                    <Container component="main" maxWidth="sm" sx={{mb: 4}}>
+                        <Paper variant="outlined" sx={{my: {xs: 3, md: 6}, p: {xs: 2, md: 3}}}>
+                            <Typography component="h1" variant="h4" align="center">
+                                Checkout
+                            </Typography>
+                            <Stepper activeStep={activeStep} sx={{pt: 3, pb: 5}}>
+                                {steps.map((label) => (
+                                    <Step key={label}>
+                                        <StepLabel>{label}</StepLabel>
+                                    </Step>
+                                ))}
+                            </Stepper>
+                            {activeStep === steps.length ? (
+                                <React.Fragment>
+                                    <Typography variant="h5" gutterBottom>
+                                        Thank you for your order.
+                                    </Typography>
+                                    <Typography variant="subtitle1">
+                                        Your order number is #2001539. We have emailed your order
+                                        confirmation, and will send you an update when your order has
+                                        shipped.
+                                    </Typography>
+                                </React.Fragment>
+                            ) : (
+                                <React.Fragment>
+                                    {getStepContent(activeStep, handleInputChange, formData)}
+                                    <Box sx={{display: 'flex', justifyContent: 'flex-end'}}>
+                                        {activeStep !== 0 && (
+                                            <Button onClick={handleBack} sx={{mt: 3, ml: 1}}>
+                                                Back
+                                            </Button>
+                                        )}
 
-                                    <Button
-                                        variant="contained"
-                                        onClick={handleNext}
-                                        sx={{mt: 3, ml: 1}}
-                                    >
-                                        {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
-                                    </Button>
-                                </Box>
-                            </React.Fragment>
-                        )}
-                    </Paper>
-                    <Copyright/>
-                </Container>
-            </>
+                                        <Button
+                                            variant="contained"
+                                            onClick={handleNext}
+                                            sx={{mt: 3, ml: 1}}
+                                        >
+                                            {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
+                                        </Button>
+                                    </Box>
+                                </React.Fragment>
+                            )}
+                        </Paper>
+                        <Copyright/>
+                    </Container>
+                </>
             }
         </>
     );
